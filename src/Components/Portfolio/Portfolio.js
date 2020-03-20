a9f0a05c9b4bc7ab2aec6ecf1b9ff9d2f2061e63
Courtney Montgomery <courtdmontgomery@gmail.com>
2020-03-20 12:57:15 -0500

    finished responsive media queries

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -16,36 +16,36 @@ class Portfolio extends Component {
       <Section className='portfolio__section' id='portfolio'>
         <Header className='portfolio__header'>
           <h1 className='portfolio__heading'>Portfolio</h1>
-          <Main className='portfolio__content_container'>
-            <p className='portfolio__content'>
-              Below you will find a few of my projects, along with the tools &
-              technologies used to build them. More details, as well as other
-              projects, can be found on my{" "}
-              <a
-                href='http://github.com/courtneydm'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                Github page
-              </a>
-              . Resume available upon request.
-            </p>
-            <div className='portfolio__projects'>
-              {this.state.projects.map((project, item) => {
-                return (
-                  <Figure
-                    key={item}
-                    heading={project.title}
-                    imgSrc={project.img}
-                    alt={project.title}
-                    url={project.url}
-                    figcaption={project.caption}
-                  />
-                );
-              })}
-            </div>
-          </Main>
         </Header>
+        <Main className='portfolio__content_container'>
+          <p className='portfolio__content'>
+            Below you will find a few of my projects, along with the tools &
+            technologies used to build them. More details, as well as other
+            projects, can be found on my{" "}
+            <a
+              href='http://github.com/courtneydm'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Github page
+            </a>
+            . Resume available upon request.
+          </p>
+          <div className='portfolio__projects'>
+            {this.state.projects.map((project, item) => {
+              return (
+                <Figure
+                  key={item}
+                  heading={project.title}
+                  imgSrc={project.img}
+                  alt={project.title}
+                  url={project.url}
+                  figcaption={project.caption}
+                />
+              );
+            })}
+          </div>
+        </Main>
       </Section>
     );
   }
