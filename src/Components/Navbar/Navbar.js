783c7906e5a7e49d94bb4ba8784b09dd25a1eb5c
Courtney Montgomery <courtdmontgomery@gmail.com>
2020-03-20 18:43:10 -0500

    update to min-width for active navbar link

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -48,7 +48,7 @@ class Navbar extends Component {
     }
     if (this.state.width >= 768) {
       skills = 495;
-      portfolio = 1090;
+      portfolio = 1060;
       contact = 3350;
     }
     if (this.state.width >= 992) {
