bd47c20657484ce97ff22f87e835353a63daa56b
Courtney Montgomery <courtdmontgomery@gmail.com>
2020-03-21 07:06:19 -0500

    refactored code for navbar active links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,25 +5,7 @@ import { List, ListItem } from "../List";
 import Header from "../Header";
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: ""
-    };
-  }
-  componentDidMount() {
-    this.setState({
-      width: window.visualViewport.width
-    });
-  }
-  setActiveLink = menu_link => {
-    let active_links = document.querySelectorAll(".navbar__list_link");
-    active_links.forEach(link => {
-      link.setAttribute("class", "navbar__list_link");
-    });
-    menu_link.setAttribute("class", "navbar__list_link active");
-  };
-
+  // Display navbar when hamburger menu is toggled
   onMenuToggle = () => {
     let navbar = document.getElementById("nav");
 
@@ -34,82 +16,7 @@ class Navbar extends Component {
     }
   };
 
-  onPageScroll = () => {
-    let contact, portfolio, skills;
-    if (this.state.width >= 320) {
-      skills = 795;
-      portfolio = 2015;
-      contact = 4090;
-    }
-    if (this.state.width >= 480) {
-      skills = 595;
-      portfolio = 1475;
-      contact = 3840;
-    }
-    if (this.state.width >= 768) {
-      skills = 495;
-      portfolio = 1120;
-      contact = 3350;
-    }
-    if (this.state.width >= 992) {
-      skills = 420;
-      portfolio = 1040;
-      contact = 2315;
-    }
-    if (this.state.width >= 1200) {
-      skills = 450;
-      portfolio = 1170;
-      contact = 2590;
-    }
-    if (this.state.width >= 1536) {
-      skills = 728;
-      portfolio = 1605;
-      contact = 2735;
-    }
-    // use window.visualViewPort.width to help determine the value of window.scrollY
-    let link;
-    if (window.scrollY >= 0 && window.scrollY < skills) {
-      link = document.getElementById("nav_about");
-      this.setActiveLink(link);
-    }
-    if (window.scrollY >= skills && window.scrollY < portfolio) {
-      link = document.getElementById("nav_skills");
-      this.setActiveLink(link);
-    }
-    if (window.scrollY >= portfolio && window.scrollY < contact) {
-      link = document.getElementById("nav_portfolio");
-      this.setActiveLink(link);
-    }
-    if (window.scrollY >= contact) {
-      link = document.getElementById("nav_contact");
-      this.setActiveLink(link);
-    }
-  };
   render() {
-    /* 
-    Skills
-    480px: 647
-    768px: 520
-    992px: 498
-    1200px: 535
-    1536px: 824
-
-    Portfolio
-    480px: 1528
-    768px: 1229
-    992px: 1124
-    1200px: 1255
-    1536px: 1699
-
-    Contact
-    480px: 3891
-    768px: 3507
-    992px: 2397
-    1200px: 2658
-    1536px: 2829
-    
-    */
-    document.addEventListener("scroll", this.onPageScroll);
     return (
       <Header className='site__header'>
         <nav className='navbar'>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,38 @@ import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
 class App extends React.Component {
+  // Set navbar active links on page scroll
+  setActiveLink = menu_link => {
+    let active_links = document.querySelectorAll(".navbar__list_link");
+    active_links.forEach(link => {
+      link.setAttribute("class", "navbar__list_link");
+    });
+    menu_link.setAttribute("class", "navbar__list_link active");
+  };
+
+  // Event handler for page scroll
+  onPageScroll = () => {
+    // Get offsetTop for each section
+    let about = document.querySelector("#about").offsetTop,
+      skills = document.querySelector("#skills").offsetTop,
+      portfolio = document.querySelector("#portfolio").offsetTop,
+      contact = document.querySelector("#contact").offsetTop;
+
+    // Get the current Y position of the window
+    let scrollYPos = window.scrollY;
+
+    // Check the current Y position and set the active link based on each section's offsetTop
+    if (scrollYPos >= about && scrollYPos < skills) {
+      this.setActiveLink(document.getElementById("nav_about"));
+    } else if (scrollYPos >= skills && scrollYPos < portfolio) {
+      this.setActiveLink(document.getElementById("nav_skills"));
+    } else if (scrollYPos >= portfolio && scrollYPos < contact) {
+      this.setActiveLink(document.getElementById("nav_portfolio"));
+    } else this.setActiveLink(document.getElementById("nav_contact"));
+  };
+
   render() {
+    document.addEventListener("scroll", this.onPageScroll);
     return (
       <Fragment>
         <Navbar />
